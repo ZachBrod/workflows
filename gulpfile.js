093f51826a09cc052f3c3065b164dfdbaa615bb6
Zach Broderick <ZPBroderick@gmail.com>
2017-01-25 17:59:33 -0700

    Added static file monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,15 @@ var concat = require('gulp-concat');
 var connect = require('gulp-connect');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
+var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
 var jsSources = [
 	'components/scripts/rclick.js',
 	'components/scripts/pixgrid.js',
 	'components/scripts/tagline.js',
 	'components/scripts/template.js'
 ];
-
-var sassSources = ['components/sass/style.scss'];
 
 gulp.task('coffee', function() {
 	gulp.src(coffeeSources)
@@ -42,9 +43,21 @@ gulp.task('compass', function() {
 		.pipe(connect.reload())
 });
 
+gulp.task('html', function() {
+	gulp.src(htmlSources)
+		.pipe(connect.reload())
+});
+
+gulp.task('json', function() {
+	gulp.src(jsonSources)
+		.pipe(connect.reload())
+});
+
 gulp.task('watch', function() {
 	gulp.watch(coffeeSources, ['coffee']);
 	gulp.watch(jsSources, ['js']);
+	gulp.watch(htmlSources, ['html']);
+	gulp.watch(jsonSources, ['json']);
 	gulp.watch('components/sass/*.scss', ['compass']);
 });
 
@@ -55,4 +68,4 @@ gulp.task('connect', function() {
 	});
 });
 
-gulp.task('default', ['coffee', 'js', 'compass', 'connect', 'watch']);
+gulp.task('default', ['html', 'coffee', 'js', 'compass', 'connect', 'watch']);
